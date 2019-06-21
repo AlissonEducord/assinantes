6ebfb32b8cf54f0df13b59ebcfd30a034b3f1305
maurilio <mauriliosfc@gmail.com>
2019-06-21 12:34:27 -0300

    acerto de query

--- models/User.js
+++ models/User.js
@@ -34,7 +34,7 @@ module.exports = class User{
                 WHERE id = ${msg.id}; 
                                                 
                 UPDATE assinantes 
-                SET qtd_mensagens = (SELECT assinantes.qtd_mensagens FROM assinantes   WHERE id = ${msg.id}) + 1            
+                SET qtd_mensagens = (SELECT COALESCE(assinantes.qtd_mensagens,0) FROM assinantes   WHERE id = ${msg.id}) + 1            
                 WHERE id = ${msg.id} ;
 
                 `, { type: Sequelize.QueryTypes.INSERT})
