32225422deb36121a14f140dc9e0d8c37917e6d6
maurilio <mauriliosfc@gmail.com>
2019-06-21 11:15:57 -0300

    configurando conexao de producao

--- config/database.js
+++ config/database.js
@@ -1,14 +1,22 @@
 const Sequelize = require('sequelize');
 
-module.exports =  new Sequelize('MetFlix', 'postgres', '123', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: false,
+// module.exports =  new Sequelize('MetFlix', 'postgres', '123', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+// });
+
+module.exports = new  Sequelize('dashgtf1bdd1c4', 'beghivwpbbvyed', 'd1fa7938af11f91f9a2525f2f7a8e2c6ec463d8fb214d0a1904089979efabc33', {
+  host: 'ec2-174-129-27-3.compute-1.amazonaws.com',
+  dialect: 'postgres',
+  "dialectOptions": {
+    "ssl": true
+  }
 });
